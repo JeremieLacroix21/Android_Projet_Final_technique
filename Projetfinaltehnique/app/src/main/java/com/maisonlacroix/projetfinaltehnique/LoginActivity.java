ca789137a857f35b60e6b1361667a394d4d1b84a
Charles Bourgeois <charlesbourgeois@live.ca>
2019-10-31 22:52:22 -0400

    login

--- Projetfinaltehnique/app/src/main/java/com/maisonlacroix/projetfinaltehnique/LoginActivity.java
+++ Projetfinaltehnique/app/src/main/java/com/maisonlacroix/projetfinaltehnique/LoginActivity.java
@@ -4,18 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.maisonlacroix.projetfinaltehnique.R;
+
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,27 +34,29 @@ import java.net.URL;
 
 public class LoginActivity extends Activity {
 
-    private String url = "http://3.15.151.13";
+    private String url = "http://3.15.151.13/Laravel/api/login";
     private EditText Username;
     private EditText Password;
     private TextView ErreurText;
     private Button loginButton;
+    private RequestQueue queue;
+    private CheckBox Sesouvenir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        queue = Volley.newRequestQueue(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         loginButton = (Button) findViewById(R.id.btn_signup);
         Username = (EditText) findViewById(R.id.input_username);
         Password = (EditText) findViewById(R.id.input_password);
         ErreurText = (TextView) findViewById(R.id.Error);
+        Sesouvenir = (CheckBox) findViewById(R.id.ch_rememberme);
     }
 
     public void Connection(View vue) {
         final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
                 R.style.Theme_AppCompat_DayNight_Dialog);
-        RequestQueue MyRequestQueue = null;
-        StringRequest MyStringRequest = null;
         if (validate()) {
             final String user = Username.getText().toString();
             final String password = Password.getText().toString();
@@ -53,28 +64,47 @@ public class LoginActivity extends Activity {
             progressDialog.setMessage("Connection...");
             progressDialog.show();
             //Connection
-            MyRequestQueue = Volley.newRequestQueue(this);
-            MyStringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            StringRequest jsonObjRequest = new StringRequest(
+                    Request.Method.POST, url, new Response.Listener<String>() {
                 @Override
                 public void onResponse(String response) {
-                    //This code is executed if the server responds, whether or not the response contains data.
-                    //The String 'response' contains the server's response.
-                    Log.d("","youpi");
+                    if(Sesouvenir.isChecked())
+                    {
+                        //Se souvenir de moi
+                        SharedPreferences reglages = getPreferences(0);
+                        SharedPreferences.Editor editeur = reglages.edit();
+                        editeur.putString("user", response);
+                        editeur.commit();
+                    }
+                    Toast.makeText(LoginActivity.this,response.toString(),Toast.LENGTH_LONG).show();
                 }
-            }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
+            },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            if(error.networkResponse.statusCode == 401)
+                            {
+                                ErreurText.setText("Les informations entrées sont invalides");
+                            }
+                            else if(error.networkResponse.statusCode == 402)
+                            {
+                                ErreurText.setText("Votre compte n'est pas encore validé");
+                            }
+                        }
+                    }) {
                 @Override
-                public void onErrorResponse(VolleyError error) {
-                    ErreurText.setText(error.toString());
+                public String getBodyContentType() {
+                    return "application/x-www-form-urlencoded; charset=UTF-8";
                 }
-            }) {
-                protected Map<String, String> getParams() {
-                    Map<String, String> MyData = new HashMap<String, String>();
-                    MyData.put("nomutilisateur", user);//Add the data you'd like to send to the server.
-                    MyData.put("motdepasse", password);
-                    return MyData;
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("name", user);
+                    params.put("password", password);
+                    return params;
                 }
             };
-            MyRequestQueue.add(MyStringRequest);
+            queue.add(jsonObjRequest);
         }
         progressDialog.dismiss();
     }
