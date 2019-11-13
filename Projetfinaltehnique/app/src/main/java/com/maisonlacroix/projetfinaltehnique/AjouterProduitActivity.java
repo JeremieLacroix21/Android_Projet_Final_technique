c2ee58ec73826ec71072499e59500b19224eb038
JeremieLacroix21 <Jrmie@DESKTOP-JL90G99>
2019-11-13 13:15:56 -0500

    MainMenu finished

--- Projetfinaltehnique/app/src/main/java/com/maisonlacroix/projetfinaltehnique/AjouterProduitActivity.java
+++ Projetfinaltehnique/app/src/main/java/com/maisonlacroix/projetfinaltehnique/AjouterProduitActivity.java
@@ -2,7 +2,9 @@ package com.maisonlacroix.projetfinaltehnique;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AjouterProduitActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class AjouterProduitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajouter_produit);
+    }
+
+    public void RedirectToMainMenu(View view)
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- Projetfinaltehnique/app/src/main/java/com/maisonlacroix/projetfinaltehnique/CompagnieInscriteActivity.java
+++ Projetfinaltehnique/app/src/main/java/com/maisonlacroix/projetfinaltehnique/CompagnieInscriteActivity.java
@@ -2,7 +2,9 @@ package com.maisonlacroix.projetfinaltehnique;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CompagnieInscriteActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class CompagnieInscriteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compagnie_inscrite);
+    }
+    public void RedirectToMainMenu(View view)
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
