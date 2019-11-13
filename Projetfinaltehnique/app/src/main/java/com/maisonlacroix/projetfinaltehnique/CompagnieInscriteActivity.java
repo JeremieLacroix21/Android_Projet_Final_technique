298d8d07e1a52ace611815147a87efb14989dcb0
JeremieLacroix21 <Jrmie@DESKTOP-JL90G99>
2019-11-12 19:38:00 -0500

    ajout d'activity

---
+++ Projetfinaltehnique/app/src/main/java/com/maisonlacroix/projetfinaltehnique/CompagnieInscriteActivity.java
@@ -0,0 +1,14 @@
+package com.maisonlacroix.projetfinaltehnique;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompagnieInscriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compagnie_inscrite);
+    }
+}
